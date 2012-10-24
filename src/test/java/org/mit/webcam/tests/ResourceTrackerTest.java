189accff701cdec7e5fd4c9208f191415302dd60
Greg Echelberger <gechelberger@gmail.com>
2012-10-24 15:07:54 -0400

    test stubs

---
+++ src/test/java/org/mit/webcam/tests/ResourceTrackerTest.java
@@ -0,0 +1,19 @@
+package org.mit.webcam.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ResourceTrackerTest {
+	
+	@Before
+	public void setup() {
+		
+	}
+	
+	@Test
+	public void testUpload() {
+		assertEquals("TODO:", (Integer)1, (Integer)1);
+	}
+}
---
+++ src/test/java/org/mit/webcam/tests/UploaderTest.java
@@ -0,0 +1,19 @@
+package org.mit.webcam.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UploaderTest {
+	
+	@Before
+	public void setup() {
+		
+	}
+	
+	@Test
+	public void testUpload() {
+		assertEquals("TODO:", (Integer)1, (Integer)1);
+	}
+}
